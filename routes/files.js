06bf37080dbbb68a889ff8660bc6923c091e047d
Hariniha <harinikannan302004@gmail.com>
2025-08-14 13:59:14 +0530

    filechnage

--- routes/files.js
+++ routes/files.js
@@ -1,7 +1,10 @@
-// View (preview) file (for images)
-router.get('/view/:fileId', fileController.viewFile);
 const express = require('express');
 const router = express.Router();
+const fileController = require('../controllers/fileController');
+const authMiddleware = require('../middleware/authMiddleware');
+
+// View (preview) file (for images)
+router.get('/view/:fileId', fileController.viewFile);
 const fileController = require('../controllers/fileController');
 const authMiddleware = require('../middleware/authMiddleware');
 
